291fa875c1ae328cc38c309f37aa5a5da9566668
NahirCarrera <ndcarrera2@espe.edu.ec>
2024-05-16 22:41:10 -0500

    Aplicación Base V1.0.0

---
+++ function.js
@@ -0,0 +1,71 @@
+const petData = {
+    adultos: [
+        { src: "img/A-Brownie.png", alt: "Brownie", age: "10 Años", name: "Brownie", gender: "Hembra", size: "Pequeña" },
+        { src: "img/A-Bruno.png", alt: "Bruno", age: "8 Años", name: "Bruno", gender: "Macho", size: "Grande" },
+        { src: "img/A-Dobby.png", alt: "Dobby", age: "9 Años", name: "Dobby", gender: "Macho", size: "Grande" },
+        { src: "img/A-Leo.png", alt: "Leo", age: "12 Años", name: "Leo", gender: "Macho", size: "Grande" }
+    ],
+    jovenes: [
+        
+        { src: "img/J-Ben.png", alt: "Ben", age: "3 Años", name: "Ben", gender: "Macho", size: "Grande" },
+        { src: "img/J-Cookie.png", alt: "Cookie", age: "3 Años", name: "Cookie", gender: "Macho", size: "Mediano" },
+        { src: "img/J-Elvys.png", alt: "Elvys", age: "2 Años", name: "Elvys", gender: "Macho", size: "Grande" },
+        { src: "img/J-Luna.png", alt: "Luna", age: "4 Años", name: "Luna", gender: "Hembra", size: "Grande" },
+        { src: "img/J-Princess.png", alt: "Princess", age: "2 Años", name: "Princess", gender: "Hembra", size: "Pequeña" },
+        { src: "img/J-Rex.png", alt: "Rex", age: "3 Años", name: "Rex", gender: "Macho", size: "Mediano" },
+        { src: "img/J-Teo.png", alt: "Teo", age: "2 Años", name: "Teo", gender: "Macho", size: "Mediano" }
+    ],
+    cachorros: [
+       
+        { src: "img/C-Axel.png", alt: "Axel", age: "5 Meses", name: "Axel", gender: "Macho", size: "Mediano" },
+        { src: "img/C-Betty.png", alt: "Betty", age: "6 Meses", name: "Betty", gender: "Hembra", size: "Mediana" },
+        { src: "img/C-Colin.png", alt: "Colin", age: "4 Meses", name: "Colin", gender: "Macho", size: "Mediana" },
+        { src: "img/C-Honguito.png", alt: "Honguito", age: "3 Meses", name: "Honguito", gender: "Macho", size: "Mediano" },
+        { src: "img/C-Kiko.png", alt: "Kiko", age: "5 Meses", name: "Kiko", gender: "Macho", size: "Mediano" },
+        { src: "img/C-Lexi.png", alt: "Lexi", age: "10 Meses", name: "Lexi", gender: "Hembra", size: "Mediano" },
+        { src: "img/C-Piper.png", alt: "Piper", age: "4 Meses", name: "Piper", gender: "Hembra", size: "Mediano" }
+    ]
+};
+
+const containerPets = document.getElementById('container-pets');
+const options = document.querySelectorAll('.container-options span');
+
+function loadPets(category) {
+    containerPets.innerHTML = ''; // Clear current pets
+    petData[category].forEach(pet => {
+        const petCard = `
+            <div class="card-pet">
+                <div class="container-img">
+                    <img src="${pet.src}" alt="${pet.alt}" />
+                    <span class="age-tag">${pet.age}</span>
+                </div>
+                <div class="content-card-pet">
+                    <div class="hearts">
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>${pet.name}</h3>
+                    <span class="adopt-button">
+                        <i class="fa-solid fa-heart"></i>
+                    </span>
+                    <p class="details">${pet.gender}<span>${pet.size}</span></p>
+                </div>
+            </div>
+        `;
+        containerPets.innerHTML += petCard;
+    });
+}
+
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        document.querySelector('.container-options .active').classList.remove('active');
+        option.classList.add('active');
+        loadPets(option.id);
+    });
+});
+
+// Load default category (adultos) on page load
+loadPets('adultos');
